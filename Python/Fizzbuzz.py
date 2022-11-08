d6c4eba2332d4675344524eb5178d294e2b6bda2
Ray Omer <rashidomer34@gmail.com>
2022-11-08 02:37:49 +0000

    Fizzbuzz Function
    First try at making a fizzbuzz game player. Currently won't output for fizz for some reason tho

---
+++ Python/Fizzbuzz.py
@@ -0,0 +1,14 @@
+def fizzbuzz():                       # Creating a function to play the game Fizzbuzz. Not concerned with optimisation just yet.
+    x = list(range(1, 101, 1))
+    for x in x:
+        i = ''
+        if x % 3 == 0:
+            i += 'fizz'
+        if x % 5 == 0:
+            i += 'buzz'
+        else:
+            i = x
+        print(i)
+
+if __name__ == "__main__":
+    fizzbuzz()
